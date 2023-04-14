63765a6e542399a4a4ac5c5171f58c2267bacfeb
赤阳子 <xuhuanli2017@gmail.com>
2023-04-14 10:04:24 +0800

    init: 导入library

---
+++ rvadapter/src/main/java/cn/bingoogolapple/baseadapter/vbinding/VBindingAdapter.kt
@@ -0,0 +1,8 @@
+package cn.bingoogolapple.baseadapter.vbinding
+
+/**
+ * Copyright (c) 2023-04, 甘草医生
+ * All rights reserved
+ * Author: xuhuanli2017@gmail.com
+ * TODOLIST 扩展当前库来支持ViewBinding
+ */
